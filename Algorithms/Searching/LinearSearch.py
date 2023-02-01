4047c5111fdcbbe18ae2b5802957264abc31b527
Ritheesh Kumar <114294870+culp-rits@users.noreply.github.com>
2023-02-01 14:21:37 +0530

    Create LinearSearch.py

---
+++ Algorithms/Searching/LinearSearch.py
@@ -0,0 +1,8 @@
+def linearsearch(array,x):
+    for i in array:
+    #traversing all elements
+        if(i==x):
+            return "Found"
+            #element found
+    return "Not Found"
+    #element not found
