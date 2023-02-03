2dff469cfcd7017dec5e6542194ba828ff206f00
Ritheesh Kumar <114294870+culp-rits@users.noreply.github.com>
2023-02-04 00:30:26 +0530

    Create LinkedList.py

---
+++ Data-Structures/LinkedLists/LinkedList.py
@@ -0,0 +1,66 @@
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class linkedlist:
+    def __init__(self):
+        self.cursor=None
+    def insertfront(self,data):
+        if(self.cursor==None):
+            self.cursor=node(data)
+        else:
+            temp=node(data)
+            temp.next=self.cursor
+            self.cursor=temp
+    def insertend(self,data):
+        temp=self.cursor
+        while(temp!=None):
+            if(temp.next==None):
+                temp.next=node(data)
+                break
+            temp=temp.next
+    def insertpos(self,data,pos):
+        temp=self.cursor
+        while(pos-2):
+            pos-=1
+            temp=temp.next
+        tmp=node(data)
+        tmp.next=temp.next
+        temp.next=tmp
+    def deletefront(self):
+        self.cursor=self.cursor.next
+    def deletend(self):
+        temp=self.cursor
+        tmp=self.cursor.next
+        while(tmp.next!=None):
+            tmp=tmp.next
+            temp=temp.next
+            if(tmp.next==None):
+                temp.next=None
+    def deletedata(self,data):
+        temp=self.cursor
+        if(temp.data==data):
+            self.cursor=self.cursor.next
+        else:
+            tmp=self.cursor.next
+            while(tmp!=None):
+                if(tmp.data==data):
+                    temp.next=tmp.next
+                    break
+                tmp=tmp.next
+                temp=temp.next
+    def search(self,data):
+        temp=self.cursor
+        i=1
+        while(temp!=None):
+            if(temp.data==data):
+                print(i)
+                break
+            i+=1
+            temp=temp.next
+    def display(self):
+        temp=self.cursor
+        while(temp!=None):
+            print(temp.data)
+            temp=temp.next
