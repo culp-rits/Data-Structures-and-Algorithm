86dd70bdb9b5827b72f9960a7ad31a2498aab255
Ritheesh Kumar <114294870+culp-rits@users.noreply.github.com>
2023-02-03 23:50:09 +0530

    Update InsertionSort.py

--- Algorithms/Sorting/InsertionSort.py
+++ Algorithms/Sorting/InsertionSort.py
@@ -1,11 +1,8 @@
 def insertionsort(array):
     for i in range(1,len(array)):
-    #traversing all elements
         k=array[i]
         j=i
         while(j>0 and k<array[j-1]):
-        #finding the correct insertion of element
             array[j]=array[j-1]
             j-=1
         array[j]=k
-        #inserting element
