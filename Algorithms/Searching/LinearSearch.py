4d09b8722e1c709583498b303147adf0370552f2
Ritheesh Kumar <114294870+culp-rits@users.noreply.github.com>
2023-02-03 23:49:31 +0530

    Update LinearSearch.py

--- Algorithms/Searching/LinearSearch.py
+++ Algorithms/Searching/LinearSearch.py
@@ -1,8 +1,5 @@
 def linearsearch(array,x):
     for i in array:
-    #traversing all elements
         if(i==x):
             return "Found"
-            #element found
     return "Not Found"
-    #element not found
