d4188c1be31a5b56676c55695cd24b85365e7b05
Ritheesh Kumar <114294870+culp-rits@users.noreply.github.com>
2023-02-03 23:48:59 +0530

    Update BinarySearch.py

--- Algorithms/Searching/BinarySearch.py
+++ Algorithms/Searching/BinarySearch.py
@@ -1,17 +1,11 @@
 def binarysearch(array,low,high,x):
     if(high>=low):
-    #making sure the element is not searched
         mid=low+(high-low)//2
-        #computing mid element to be searched
         if(array[mid]>x):
             return binarysearch(array,low,mid-1,x)
-            #recursive call on left subarray 
         elif(array[mid]<x):
             return binarysearch(array,mid+1,high,x)
-            #recursive call on right subarray
         else:
             return "Found"
-            #element found
     else:
         return "Not Found"
-        #element is not present
