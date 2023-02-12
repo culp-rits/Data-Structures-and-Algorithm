4bfee29d13e2021f2e30b3d8b176d80779f96f55
Ritheesh Kumar <114294870+culp-rits@users.noreply.github.com>
2023-02-12 20:35:32 +0530

    Create BellmanFord.py

---
+++ Algorithms/ShortestPath/BellmanFord.py
@@ -0,0 +1,11 @@
+import sys
+
+def bellmanford(graph,n,src):
+    dist = [sys.maxsize]*n
+    dist[src] = 0
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if(graph[i][j] != INF and dist[i] != INF and dist[i] + graph[i][j] < dist[j]):
+                    dist[j] = dist[i] + graph[i][j]
+    print(dist)
